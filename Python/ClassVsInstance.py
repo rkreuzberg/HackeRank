0d1537da5ed9e898a2b3147ddb9f98ec4e79571f
Rodrigo A. Kreuzberg <rodrigokreuzberg@gmail.com>
2020-05-09 23:52:33 -0400

    Modificações de código

--- Python/ClassVsInstance.py
+++ Python/ClassVsInstance.py
@@ -1,17 +1,36 @@
 class Person:
-    def __init__(self,initialAge):
+    def __init__(self, initialAge):
         # Add some more code to run some checks on initialAge
-    def amIOld(self):
+        initialAge = 0
+
+    def amIOld(self, age):
         # Do some computations in here and print out the correct statement to the console
-    def yearPasses(self):
-        # Increment the age of the person in here
+        # print("amIold", age)
+        if age <= 0:
+            print("Age is not valid, setting age to 0.")
+            # age = 0
+        elif (age > 13 and age <=18):
+            print("You are young.")
+        else:
+            print("You are a teenager.")
+    def yearPasses(self, age):
+        #Increment the age of the person in here
+        self = age + 1
+        print("yearPasses")
 
 t = int(input())
+
 for i in range(0, t):
     age = int(input())
     p = Person(age)
-    p.amIOld()
+    p.amIOld(age)
     for j in range(0, 3):
-        p.yearPasses()
-    p.amIOld()
-    print("")
+        p.yearPasses(age)
+        # print("range2")
+    p.amIOld(age)
+    # print("range1")
+
+
+#Age is not valid, setting age to 0. (age < 0)
+#You are young. (age >= 13 and age < 18)
+#You are a teenager. (age > 18)
