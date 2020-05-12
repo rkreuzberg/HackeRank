89a2b6d4fdad16eb99932c67de5e7ff3b5470e9d
Rodrigo A. Kreuzberg <rodrigokreuzberg@gmail.com>
2020-05-11 20:51:18 -0400

    Next exercise

---
+++ Python/Loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    x = 1
+while x <= 10:
+    print (n,"x",x,"=",n*x)
+    x += 1
