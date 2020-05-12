4c0288e553dcbf397563fe3796eb3b5d10a79850
Rodrigo A. Kreuzberg <rodrigokreuzberg@gmail.com>
2020-05-11 20:39:28 -0400

    Finish exercise

--- Python/ClassVsInstance.py
+++ Python/ClassVsInstance.py
@@ -1,34 +1,43 @@
 class Person:
     def __init__(self, initialAge):
-        # Add some more code to run some checks on initialAge
-        initialAge = 0
-
-    def amIOld(self, age):
-        # Do some computations in here and print out the correct statement to the console
-        # print("amIold", age)
-        if age <= 0:
+        if (initialAge < 0):
             print("Age is not valid, setting age to 0.")
-            # age = 0
-        elif (age > 13 and age <=18):
-            print("You are young.")
+            self.age = 0
         else:
+            self.age = initialAge
+
+    def amIOld(self):
+        if self.age >= 18:
+            print("You are old.")
+        elif self.age >= 13:
             print("You are a teenager.")
-    def yearPasses(self, age):
-        #Increment the age of the person in here
-        self = age + 1
-        print("yearPasses")
+        else:  # age < 13
+            print("You are young.")
+
+    def yearPasses(self):
+        self.age += 1
 
 t = int(input())
-
 for i in range(0, t):
     age = int(input())
     p = Person(age)
-    p.amIOld(age)
+    p.amIOld()
     for j in range(0, 3):
-        p.yearPasses(age)
-        # print("range2")
-    p.amIOld(age)
-    # print("range1")
+        p.yearPasses()
+    p.amIOld()
+    print("")
+
+#
+#
+# for i in range(0, t):
+#     age = int(input())
+#     p = Person(age)
+#     p.amIOld(age)
+#     for j in range(0, 3):
+#         p.yearPasses(age)
+#         # print("range2")
+#     p.amIOld(age)
+#     # print("range1")
 
 
 #Age is not valid, setting age to 0. (age < 0)
